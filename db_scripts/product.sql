b9c364b0f468d9bb4fec4ee7673e0c6388a88ece
kay-apao25 <kayapao@yahoo.com>
2015-04-14 14:31:49 +0800

    [TASK] Delete -quantity- column from product table

--- db_scripts/product.sql
+++ db_scripts/product.sql
@@ -4,7 +4,6 @@ create table product{
     supplier_name text,
     supplier_address text,
     serial_number char,
-    quantity, bigint,
     model text,
     amount numeric,
     description text,
@@ -14,7 +13,7 @@ create table product{
 -- HOW TO USE:
 -- SELECT add_prodprof('computer', 'Iligan City', 'Keira Montiel', 'none', 'lala', 3, 1326.88);
 create or replace function add_prodprof(p_item_name text, p_supplier_address text, p_supplier_name text, p_serial_number char, 
-																		p_description text,	p_model text, p_quantity bigint, p_amount numeric)
+																		p_description text,	p_model text, p_amount numeric)
     returns text as
 $$
   declare
@@ -26,13 +25,13 @@ $$
   		where supplier_name = p_supplier_name and serial_number = p_serial_number and model = p_model;
 
      if v_supplier_name isnull and v_serial_number isnull and v_model isnull then
-      insert into product(item_name, supplier_name, supplier_address, serial_number, quantity, model, amount)
-      		values (p_item_name, p_supplier_name, p_supplier_address, p_serial_number, p_quantity, p_model, p_amount);
+      insert into product(item_name, supplier_name, supplier_address, serial_number, model, amount)
+      		values (p_item_name, p_supplier_name, p_supplier_address, p_serial_number, p_model, p_amount);
       return 'Successfully added';
     else
       update product
       
-      set item_name = p_item_name, supplier_address = p_supplier_address, quantity = p_quantity, amount = p_amount
+      set item_name = p_item_name, supplier_address = p_supplier_address, amount = p_amount
         where supplier_name = p_supplier_name and serial_number = p_serial_number and model = p_model;
       
       return 'Successfully updated';
