7e184076db8b006517e1f8c3f279327f9956a364
JennieRamos <jennieramos06@gmail.com>
2015-04-15 16:51:38 +0800

    [TASK] modified miv add function

--- db_scripts/miv.sql
+++ db_scripts/miv.sql
@@ -13,3 +13,28 @@ create table miv(
 	doc_date date,
 	remark text
 );
+
+CREATE OR REPLACE
+  FUNCTION add_irr(p_irr_no int, p_inv_station_no char, p_asset_code int, p_dce_custodian char, p_dce_user char, p_cost_center_no int, p_wrs_num char,
+					p_quantity numeric, p_amount numeric, p_date_issued date, p_doc_date date, p_remark text)
+  RETURNS text as
+
+$BODY$
+  declare
+    v_miv_no int;
+  begin
+    select into v_miv_no miv_no from miv
+    where miv_no = v_miv_no; 
+
+    if v_asset_code isnull then
+      insert into employee(irr_no, inv_station_no, asset_code, dce_custodian , dce_user, cost_center_no, wrs_num,
+					quantity, amount , date_issued , doc_date, remark )
+      values (p_irr_no, p_inv_station_no, p_asset_code, p_dce_custodian , p_dce_user, p_cost_center_no, p_wrs_num,
+					p_quantity, p_amount , p_date_issued , p_doc_date, p_remark);
+      return 'OK';
+    else
+      return 'OK';       
+    end if;
+  end;
+$BODY$
+language 'plpgsql';
