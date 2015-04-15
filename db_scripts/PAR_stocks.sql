401963ff229781af61d00d4d45549c1b1d95c7d2
lorie <lorie@myeskwela.msuiit.edu.ph:/my.eskwela/extras/repo.git>
2015-04-15 16:46:15 +0800

    [TASK] modified functions in db_scripts/PAR_stocks.sql

--- db_scripts/PAR_stocks.sql
+++ db_scripts/PAR_stocks.sql
@@ -1,26 +1,25 @@
 create table PAR(
-  dce_FK char references employee(dce),
+  dce_FK char primary key references employee(dce),
   asset_code_FK int primary key references product(asset_code),
   par_date date,
   par_no char,
   amt_cost numeric,
-  description text,
   remark text
 );
 
 create or replace
-	function add_par(dce_ char, asset_code_ int, date_ date, par_no_ char)
+	function add_par(dce_ char, asset_code_ int, date_ date, par_no_ char, amt_cost_ numeric, remark_ text)
 	returns text as
 $$
 	declare
 		v_asset_code int;
 	begin
 		select into v_asset_code asset_code_FK from PAR where
-			asset_code_ = asset_code_FK; 
+			asset_code_ = asset_code_FK and dce_FK = dce_; 
 
 		if v_asset_code isnull then
-			insert into PAR(dce_FK, asset_code_FK, par_date) values
-				(dce_, asset_code_, now()::date);
+			insert into PAR(dce_FK, asset_code_FK, par_date, par_no, amt_cost, remark) values
+				(dce_, asset_code_, now()::date, par_no_, amt_cost_, remark_);
 			return 'OK';
 		else
 			return 'Record already exist.';
@@ -30,17 +29,17 @@ $$
 
 $$
 language 'plpgsql';
--- select add_par(dce, asset_code);
+-- select add_par(dce_, asset_code_, date_, par_no_, amt_cost_, remark_);
 
 create or replace
-	function get_par(in char, out char, out int, out date)
+	function get_par(in char, in int, out char, out int, out date)
 	returns setof record as
 
 $$
-	select * from PAR where dce_FK = $1;
+	select * from PAR where dce_FK = $1 and asset_code_FK = $2;
 $$
  language 'sql';
- --select get_par(employee);
+ --select get_par(employee, code);
 
 create or replace
  	function del_par(in char, in int)
