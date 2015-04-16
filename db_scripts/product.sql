b98d55c43e14823d173febf840990bc2446348ad
kay-apao25 <kayapao@yahoo.com>
2015-04-16 09:10:06 +0800

    [CLEAN] Deleted excess model parameter in add function

--- db_scripts/product.sql
+++ db_scripts/product.sql
@@ -32,7 +32,7 @@ create table product(
 create or replace function add_prodprof(p_nsn char, p_slc_num int, p_inv_station_no_fk char, p_cost_center_no_fk int, p_generic_name text,
   p_brand text, p_part_num char, p_manufacture_date date, p_expiry_date date, p_class char, p_stock char, p_block char, p_unit_measure char,
   p_unit_cost numeric, p_quantity numeric, p_average_amt numeric, p_status char, p_balance_limit numeric, p_item_name text, p_serial_number char,
-  p_model text, p_remark text, p_description text,	p_model text, p_amount numeric)
+  p_model text, p_remark text, p_description text,	p_amount numeric)
     return text as
 $$
   declare
@@ -45,10 +45,10 @@ $$
      if v_asset_code isnull and v_cost_center_no isnull then
       insert into product(nsn, slc_num, inv_station_no_fk, cost_center_no_fk, generic_name, brand, part_num, manufacture_date, 
             expiry_date, class, stock, block, unit_measure, unit_cost, quantity, average_amt, status, balance_limit, 
-            item_name, serial_number, model, remark, description, model, amount)
+            item_name, serial_number, model, remark, description, amount)
       		values (p_nsn, p_slc_num, p_inv_station_no_fk, p_cost_center_no_fk, p_generic_name, p_brand, p_part_num, p_manufacture_date, 
             p_expiry_date, p_class, p_stock, p_block, p_unit_measure, p_unit_cost, p_quantity, p_average_amt, p_status, p_balance_limit, 
-            p_item_name, p_serial_number, p_model, p_remark, p_description,  p_model, p_amount);
+            p_item_name, p_serial_number, p_model, p_remark, p_description,  p_amount);
       return 'Successfully added';
     else
       update product
