da787720421397c1d3566aead8daafdad451039d
lorie <lorie@myeskwela.msuiit.edu.ph:/my.eskwela/extras/repo.git>
2015-04-17 09:23:02 +0800

    [TASK] added registered models to WOS/WISH/admin.py

--- WOS/WISH/admin.py
+++ WOS/WISH/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
-from .models import Product
+from . import models
 
 # Register your models here.
+admin.site.register(Supplier)
 admin.site.register(Product)
+admin.site.register(PAR)
+admin.site.register(GARV)
+admin.site.register(Pending)
+admin.site.register(Cost_center)
+admin.site.register(Inventory_stat)
+admin.site.register(Employee)
+admin.site.register(IRR_header)
+admin.site.register(IRR)
+admin.site.register(MIV)
