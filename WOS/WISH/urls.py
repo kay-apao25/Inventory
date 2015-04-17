f2563640de3ea0f87e987d184ade1123f665d2e7
kay-apao25 <kayapao@yahoo.com>
2015-04-17 10:16:40 +0800

    [TASK] Add urls, views and html file for WRS form

--- WOS/WISH/urls.py
+++ WOS/WISH/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.employee_list),
+    url(r'^$', views.wrs_form),
 ]
--- WOS/WISH/views.py
+++ WOS/WISH/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def wrs_form(request):
+    return render(request, 'WISH/wrs_form.html', {})
