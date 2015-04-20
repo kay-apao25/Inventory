958ae9a9d5ee5f206bfcf6564432fa82098e09a6
lorie <lorie@myeskwela.msuiit.edu.ph:/my.eskwela/extras/repo.git>
2015-04-20 08:24:25 +0800

    [TASK] added some lines in WOS/WISH/urls.py

--- WOS/WISH/urls.py
+++ WOS/WISH/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.wrs_form),
+    #url(r'^$', views.wrs_form),
     url(r'^$', views.par_form),
-    url(r'^$', views.cme_form),
-    url(r'^$', views.irr_form),
-    url(r'^$', views.gatepass_form),
-    url(r'^$', views.miv_form),
+    #url(r'^$', views.cme_form),
+    #url(r'^$', views.irr_form),
+    #url(r'^$', views.gatepass_form),
+    #url(r'^$', views.miv_form),
 ]
