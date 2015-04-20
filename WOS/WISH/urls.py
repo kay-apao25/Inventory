b3a33fe482171ba691646324b6f4f1b6a0c80817
kay-apao25 <kayapao@yahoo.com>
2015-04-20 09:34:17 +0800

    [TASK] Add Customer model

--- WOS/WISH/urls.py
+++ WOS/WISH/urls.py
@@ -4,11 +4,11 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^product/new/$', views.product_new, name='product_new'),
-    url(r'^$', views.wrs_form),
-    url(r'^$', views.wrs_form),
-    url(r'^$', views.par_form),
-    url(r'^$', views.cme_form),
-    url(r'^$', views.irr_form),
-    url(r'^$', views.gatepass_form),
-    url(r'^$', views.miv_form),
+    #url(r'^$', views.wrs_form),
+    #url(r'^$', views.wrs_form),
+    #url(r'^$', views.par_form),
+    #url(r'^$', views.cme_form),
+    #url(r'^$', views.irr_form),
+    #url(r'^$', views.gatepass_form),
+    #url(r'^$', views.miv_form),
 ]
