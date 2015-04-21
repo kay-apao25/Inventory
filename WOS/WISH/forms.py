16d1f1c5d4ea81cc484ef675f83948f83179b6c2
lorie <lorie@myeskwela.msuiit.edu.ph:/my.eskwela/extras/repo.git>
2015-04-21 09:23:02 +0800

    [TASK] edited WOS/WISH/forms.py

--- WOS/WISH/forms.py
+++ WOS/WISH/forms.py
@@ -18,7 +18,7 @@ class IRR_entryForm(forms.ModelForm):
         fields = ('irr_headkey', 'inv_station_no', 'reference', 'invoice_num', 'po_num', 'dr_num', \
         	'dce_custodian', 'dce_user', 'proc_date', 'type_n', 'remark', 'date_dlvrd', 'supl_fk',)
 
-class PAR(forms.ModelForm):
+class PAR_entryForm(forms.ModelForm):
 
     class Meta:
         model = PAR
