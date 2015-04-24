39f184f8e18fd20dcf06de02c3283addac7e3bef
JennieRamos <jennierajennie@myeskwela.msuiit.edu.ph>
2015-04-24 13:52:39 +0800

    [TASK] modified

--- WOS/WISH/forms.py
+++ WOS/WISH/forms.py
@@ -6,7 +6,7 @@ class ProductForm(forms.ModelForm):
 
     class Meta:
         model = Product
-        fields = ('product_number', 'inv_station_no', 'item_name', \
+        fields = ('product_number', 'item_name', \
         	'generic_name', 'brand', 'part_number', 'manufacture_date', 'expiry_date', 'classification', 'stock', \
         	'stock', 'block', 'unit_measure', 'unit_cost', 'quantity', 'average_amount', 'status',\
         	'balance_limit', 'serial_number', 'model', 'description', 'remark', 'purchased_from',)
@@ -22,7 +22,7 @@ class IRR_entry_cont_Form(forms.ModelForm):
 
     class Meta:
         model = IRR
-        fields = ('cost_center_no' ,'quantity_accepted', 'quantity_actual','asset_code', 'date_recv' , 'wo_no' , 'remark',)
+        fields = ('cost_center_no', 'date_recv' , 'wo_no' , 'remark',)
 
 class MIV_entryForm(forms.ModelForm):
 
@@ -32,23 +32,23 @@ class MIV_entryForm(forms.ModelForm):
             'quantity': forms.TextInput(attrs={'placeholder': 'Quantity'}),
             'date_issued': forms.TextInput(attrs={'placeholder': 'Date Issued'}),
             'remark': forms.TextInput(attrs={'placeholder': 'Remark'}),
-            'asset_code': forms.TextInput(attrs={'placeholder': 'Asset Code'}),
+            #'asset_code': forms.TextInput(attrs={'placeholder': 'Asset Code'}),
             'irr_no': forms.TextInput(attrs={'placeholder': 'IRR No.'}),
             'inv_station_no': forms.TextInput(attrs={'placeholder': 'Inventory Station No.'}),
         }
-        fields = ( 'quantity', 'date_issued',  'remark' , 'asset_code', 'irr_no', 'inv_station_no',)
+        fields = ( 'quantity', 'date_issued',  'remark' , 'product', 'inv_station_no',)
 
 class PAR_entryForm(forms.ModelForm):
 
     class Meta:
         model = PAR
-        fields = ('dce', 'asset_code', 'par_no', 'amt_cost', 'remark', 'qty', 'approved_by', \
+        fields = ('dce','product', 'par_no', 'amt_cost', 'remark', 'qty', 'approved_by', \
         'issued_by', 'inv_stat_no', 'PO_number', 'date_acquired', 'wo_number', )
 
 class GARV_entryForm(forms.ModelForm):
 
     class Meta:
         model = GARV
-        fields = ('dce', 'asset_code', 'garv_no', 'cc_number', 'wo_number', 'qty',\
+        fields = ('dce', 'product', 'garv_no', 'cc_number', 'wo_number', 'qty',\
                     'par_number', 'remarks', 'inspected_by', 'date_inspected', 'confirmed_by', \
                     'date_confirmed', 'noted_by', )
