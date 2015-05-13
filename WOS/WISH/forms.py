c5499b6ed556142a84fc086c78ab19d2a76907de
kay-apao25 <kayapao@yahoo.com>
2015-05-13 11:27:23 +0800

    [TASK] Enable display of messages when there are no available records to be PAR'd or MIV'd

--- WOS/WISH/forms.py
+++ WOS/WISH/forms.py
@@ -147,6 +147,8 @@ class PAR_Form(forms.ModelForm):
             cost_center_no=IRR.objects.get(irr_no=irn).irr_headkey.inv_station_no.cost_center_no.id))"""
 
     date_acquired = forms.DateField(widget=DateTimePicker(options={"format": "YYYY-MM-DD", "pickTime": False}), label='Date acquired*', required=False)
+    dce = forms.ModelChoiceField(queryset=Employee.objects.all(), label='Accountable Employee*')
+    approved_by = forms.ModelChoiceField(queryset=Employee.objects.all(), label='Approved by*')
 
     class Meta:
         model = PAR
