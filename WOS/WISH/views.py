3567185ac86d7693f09dd212e25989cde903eeeb
JennieRamos <jennieramos06@gmail.com>
2015-05-18 11:09:18 +0800

    replace

--- WOS/WISH/views.py
+++ WOS/WISH/views.py
@@ -108,16 +108,16 @@ def employee_lib(request):
         form = Em_lib(request.POST)
         if form.is_valid() :
             employee = form.save(commit=False)
-            a = 0
-            for employee.name in employee:
-                if a <= len(employee.name):
-                    if employee.name[a] == "'":
-                        employee.name.replace("'", '-')
-                        a += 1
-                    else:
-                        a += 1
+            res = ""
+            em_name = list(form.data['name'])
+            for name in em_name:
 
-
+                if name == "'":
+                    name = '-'
+                    res = res + name
+                else:
+                    res = res + name
+            employee.name = res
             employee.save()
             msg = 'Employee was added successfully.'
 
