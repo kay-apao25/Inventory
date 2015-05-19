da730dc4ab2d67226844a4bb8721eeb86ff9d438
clorieanne <clorieanne@gmail.com>
2015-05-19 08:45:41 +0800

    [TASK] for login

--- WOS/WISH/views.py
+++ WOS/WISH/views.py
@@ -14,36 +14,35 @@ import json
 # Create your views here.
 
 def index(request):
-    product = Product.objects.order_by('id')[:3]
-    try:
-        irr = IRR.objects.latest('wrs_number')
-    except:
-        irr = None
-    try:
-        par = PAR.objects.latest('par_no')
-    except:
-        par = None
-    try:
-        garv = GARV.objects.latest('garv_no')
-    except:
-        garv = None
-    try:
-        inv = Inventory_stat.objects.latest('id')
-    except:
-        inv = None
-    try:
-        sup = Supplier.objects.latest('id')
-    except:
-        sup = None
-    try:
-        cc = Cost_center.objects.latest('id')
-    except:
-        cc = None
-
     if request.user.is_authenticated():
+        product = Product.objects.order_by('id')[:3]
+        try:
+            irr = IRR.objects.latest('wrs_number')
+        except:
+            irr = None
+        try:
+            par = PAR.objects.latest('par_no')
+        except:
+            par = None
+        try:
+            garv = GARV.objects.latest('garv_no')
+        except:
+            garv = None
+        try:
+            inv = Inventory_stat.objects.latest('id')
+        except:
+            inv = None
+        try:
+            sup = Supplier.objects.latest('id')
+        except:
+            sup = None
+        try:
+            cc = Cost_center.objects.latest('id')
+        except:
+            cc = None
         return render(request, 'WISH/index.html', {'product': product, 'irr': irr, 'par': par, 'garv': garv, 'inv': inv, 'sup': sup, 'cc': cc})
     else:
-        return render(request, 'registration/login1.html')
+        return render(request, 'registration/login.html')
 
 def aboutus(request):
     return render(request, 'WISH/AboutUs.html', {})
