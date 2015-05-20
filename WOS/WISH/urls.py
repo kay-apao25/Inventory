93ea319a9d5e3c600d775f66b32cdf2e036c53b7
kay-apao25 <kayapao@yahoo.com>
2015-05-21 07:59:46 +0800

    [TASK] Enable filter of reports to be displayed

--- WOS/WISH/urls.py
+++ WOS/WISH/urls.py
@@ -5,17 +5,28 @@ from . import views
 from . import models
 
 urlpatterns = [
-    url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
-    url(r'^wrs_form/(?P<pk>[0-9]+)/$', views.wrs_form),
     url(r'^$', views.index),
     url(r'^Aboutus/$', views.aboutus),
+
+    #URL patterns for Log In and Out (start)
+    url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
+    #URL patterns for Log In and Out (end)
+
+    #URL patterns for File and Product Fill-Up Forms (start)
+    url(r'^wrs_form/(?P<pk>[0-9]+)/$', views.wrs_form),
     url(r'^product/new/$', views.product_new, name='product_new'),
     url(r'^par/(?P<inv>[0-9]+)/$', views.par),
     url(r'^par_f/$', views.par_f),
     url(r'^irr_entry/$', views.irr_entry, name='irr_entry'),
     url(r'^miv_entry/$', views.miv_entry, name='miv_entry'),
     url(r'^miv_entry_S/(?P<pk>[0-9]+)/$', views.miv_entry_S),
+    url(r'^garv_entry_f/$', views.garv_entry_f),
+    url(r'^product_to_garv/(?P<pk>[0-9]+)/$', views.product_to_garv),
+    url(r'^wrs_entry/$', views.wrs_entry),
+    #URL patterns for File and Product Fill-Up Forms (end)
+
+    #URL patterns for File Forms (start)
     url(r'^par_form/(?P<pk>[0-9]+)/$', views.par_form),
     url(r'^product_form/(?P<pk>[0-9]+)/$', views.product_form),
     url(r'^inv_stat_form/(?P<pk>[0-9]+)/$', views.inv_stat_form),
@@ -31,16 +42,15 @@ urlpatterns = [
     url(r'^cost_center_details/(?P<pk>[0-9]+)/$', views.cost_center_details),
     url(r'^supplier_details/(?P<pk>[0-9]+)/$', views.supplier_details),
     url(r'^employee_details/(?P<dce>[0-9]+)/$', views.employee_details),
-    url(r'^garv_entry_f/$', views.garv_entry_f),
-    url(r'^product_to_garv/(?P<pk>[0-9]+)/$', views.product_to_garv),
     url(r'^garv_form/(?P<pk>[0-9]+)/$', views.garv_form),
-    url(r'^wrs_entry/$', views.wrs_entry),
     url(r'^product_to_irr/(?P<pk>[0-9]+)/(?P<inv>[0-9]+)/$', views.\
         product_to_irr),
     url(r'^irr_form/(?P<pk>[0-9]+)/$', views.irr_form),
     url(r'^miv_form/(?P<pk>[0-9]+)/$', views.miv_form),
     url(r'^add_cost_center/$', views.add_cost_center,\
      name='add_cost_center'),
+
+    #URL patterns for File and Product Reports (start)
     url(r'^irr_reports/$', lambda request: ListView.as_view(queryset=\
         models.IRR.objects.filter(irr_headkey__in=[i.id for i in (\
         models.IRRHeader.objects.filter(dce_custodian=(\
@@ -72,9 +82,15 @@ urlpatterns = [
         name=str(request.user.first_name) + ' ' + str(request.user.last_name\
         )))), context_object_name='irr_list', template_name=\
         'WISH/garv_reports.html')(request), name='garv_reports'),
-    url(r'^product_reports/$', ListView.as_view(model=models.Product, \
-        context_object_name='product_list',\
-        template_name='WISH/product_reports.html'), name='product_reports'),
+    url(r'^product_reports/$', lambda request: ListView.as_view(queryset=\
+        models.Product.objects.filter(inv_station_no__in=[i.inv_station_no \
+        for i in (models.InventoryStat.objects.filter(cost_center_no=(\
+        models.Employee.objects.get(name=str(request.user.first_name) \
+        + ' ' + str(request.user.last_name)).cost_center_no)))]),
+        context_object_name='irr_list', template_name=\
+        'WISH/product_reports.html')(request), name='product_reports'),
+    #URL patterns for File and Product Reports (start)
+
     url(r'^inv_stat/$', ListView.as_view(model=models.InventoryStat, \
         context_object_name='inv_list',\
         template_name='WISH/inv_stat.html'), name='inv_stat'),
