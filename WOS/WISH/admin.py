c6fd9b8c8eb23f326f7657a594005709a56584a6
kay-apao25 <kayapao@yahoo.com>
2015-05-25 11:00:30 +0800

    [TASK] Add neccessary lines in product_detail.html

--- WOS/WISH/admin.py
+++ WOS/WISH/admin.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from simple_history.admin import SimpleHistoryAdmin
 from WISH.models import Supplier, Product, PAR, GARV, CostCenter, \
-		InventoryStat, Employee, IRRHeader, IRR, MIV
+		InventoryStat, Employee, IRRHeader, IRR, MIV, Customer
 
 
 
@@ -16,3 +16,4 @@ admin.site.register(Employee, SimpleHistoryAdmin)
 admin.site.register(IRRHeader, SimpleHistoryAdmin)
 admin.site.register(IRR, SimpleHistoryAdmin)
 admin.site.register(MIV, SimpleHistoryAdmin)
+admin.site.register(Customer, SimpleHistoryAdmin)
