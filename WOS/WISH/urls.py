d976bc841b553c970fa763ff2ca85b82b391aa22
philip.0008 <philip.0008@yahoo.com>
2015-05-27 12:24:55 +0800

    [TASK] url for invres

--- WOS/WISH/urls.py
+++ WOS/WISH/urls.py
@@ -160,4 +160,8 @@ urlpatterns = [
 
 
     #URL patterns for Viewing Libraries (end)
+
+    url(r'^inv_stat_res/(?P<pk>[0-9]+)/$', login_required(views.inv_stat_res),\
+        name='invstat_res'),
+
 ]
